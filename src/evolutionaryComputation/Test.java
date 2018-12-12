e960d14639608edffd7082223a521c3530282b11
OPelham <oliverpelham@gmail.com>
2018-12-13 12:38:20 +1300

    completed subset selection  method

--- src/evolutionaryComputation/Test.java
+++ src/evolutionaryComputation/Test.java
@@ -14,7 +14,7 @@ public class Test {
 	static double lowerRange = 0;
 	static double upperRange = 100;
 	static ArrayList<double[]> currentGeneration = new ArrayList<>();
-	static Map<Integer, double[]> subset = new HashMap<>();  //<position in currentGeneration array, phenotype>
+	static Map<Integer, Double> subset = new HashMap<>();  //<position in currentGeneration array, phenotype>
 	
 	/**
 	 * Creates a new parent with random doubles in phenotype (double[5]) within given range
@@ -49,22 +49,46 @@ public class Test {
 	public static void pickSubset(int sampleSize) {
 		//first clear old subset
 		subset.clear();
+		//set tickets
+		ArrayList<Integer> tickets = new ArrayList<>();
+		for (int t=0; t<generationSize; t++) {
+			tickets.add(t);
+		}
 		//pick a number of random parents for subset and store in map with in
 		for (int i=0; i<sampleSize; i++) {
-			//to avoid double ups use 'tickets'
-			int[] tickets = new int[sampleSize];
-			for (int t=0; t<sampleSize; t++) {
+			//to avoid double ups use 'tickets'	
+			Random rand = new Random();			
+			int selectedPosition = (int) (tickets.size()-1 * rand.nextDouble());
+				//process ticket
+				System.out.println("debug ticket size");
+				System.out.println(tickets.size());
+				System.out.println("selected position");
+				System.out.println(selectedPosition);
+				int ticket = tickets.get(selectedPosition); //ticket is a ubique int from 0 to generation size -1
+				System.out.println(ticket);
+				//use this to get position in current gen and fitness and store in map
+
+				double[] selectedParent = currentGeneration.get(ticket);
 				
-				//to do set tcikets, chose from tickets not taken, ...
-				
-			}
+				double selectedParentFitness = OptimisationFunction.unknownFunction(selectedParent);
+				subset.put(ticket, selectedParentFitness);
+				//delete ticket
+				tickets.remove(ticket);
+					
+		} 
+		System.out.println("checking subset");
+		for(double fitness: subset.values()) {
+			System.out.println(fitness);
 		}
+		System.out.println("subset check complete");
 	}
 	
 
 	public static void main(String[] args) {
 		
 		populateFirstGeneration();
+		System.out.println("subset fitness");
+		pickSubset(10);
 		
 		
 		for(int i=0; i<100; i++) {
